4c7b709012a9195a9ff3e58570ffa86a7ce468b6
Maritza Rodriguez <maritzarg2020@gmail.com>
2023-07-04 22:53:50 -0500

    feat: set up db and swagger

--- hackatonFinal/urls.py
+++ hackatonFinal/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", include(("api.urls", "api"), namespace="api")),
+    path("admin/", admin.site.urls),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "docs/",
+        SpectacularSwaggerView.as_view(
+            template_name="swagger-ui.html", url_name="schema"
+        ),
+        name="swagger-ui",
+    ),
 ]
